c3d621806a081c3ecc150f93f93a6f24a1c01ab8
Samad Talukder <samadtalukder@users.noreply.github.com>
2018-01-19 01:20:50 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
 	    Countdown initializer
 	*/
 	var now = new Date();
-	var countTo = 150 * 24 * 60 * 60 * 1000 + now.valueOf();    
+	var countTo = 231 * 24 * 60 * 60 * 1000 + now.valueOf();    
 	$('.timer').countdown(countTo, function(event) {
 		
 		$(this).find('.days').text(event.offset.totalDays);
