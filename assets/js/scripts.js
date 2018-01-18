c8627c333ea054e12a63b20082a7c27b763a79b2
samadtalukder <samadtalukdar6@gmail.com>
2018-01-19 01:09:01 +0600

    Coming Soon Page Add

---
+++ assets/js/scripts.js
@@ -0,0 +1,40 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Wow
+    */
+    new WOW().init();
+    
+    /*
+	    Countdown initializer
+	*/
+	var now = new Date();
+	var countTo = 150 * 24 * 60 * 60 * 1000 + now.valueOf();    
+	$('.timer').countdown(countTo, function(event) {
+		
+		$(this).find('.days').text(event.offset.totalDays);
+		$(this).find('.hours').text(event.offset.hours);
+		$(this).find('.minutes').text(event.offset.minutes);
+		$(this).find('.seconds').text(event.offset.seconds);
+	});
+	
+	/*
+	    Subscription form
+	*/
+	$('.success-message').hide();
+	$('.error-message').hide();
+	
+	$('.subscribe form').submit(function(e) {
+		e.preventDefault();
+	    var postdata = $('.subscribe form').serialize();
+	    
+	});
+    
+});
+
